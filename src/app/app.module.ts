b2c0e8340fc4e2baa8e744de913273a77071d41b
Ambroise Maire <ambroise.maire7@gmail.com>
2023-10-12 09:47:33 +0200

    init: New core components
    -- Component Sidemenu --
    - Generated the component sidemenu (src/app/shared/components/sidemenu.component).
    - It's the navigation menu, made with the mat-drawer component.
    
    -- Component home --
    - Generated the component home (src/app/home/home.component).
    
    -- Other --
    - Adds to imports in app.module.ts for Material Design components.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatCardModule } from '@angular/material/card';
+import { MatSidenavModule } from '@angular/material/sidenav';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { SidemenuComponent } from './shared/components/sidemenu/sidemenu.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SidemenuComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatSidenavModule
   ],
   providers: [],
   bootstrap: [AppComponent]
