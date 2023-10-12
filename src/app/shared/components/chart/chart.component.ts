ed43709155bb1bf285fa85969c7e053d25d04106
Ambroise Maire <ambroise.maire7@gmail.com>
2023-10-12 15:18:02 +0200

    feat/dev-home: Series name
    -- Series Name --
    - Added series 'name' property in the chart options.
    - Value passed by an @Input.

--- src/app/shared/components/chart/chart.component.ts
+++ src/app/shared/components/chart/chart.component.ts
@@ -6,21 +6,25 @@ import * as Highcharts from 'highcharts';
   templateUrl: './chart.component.html'
 })
 export class ChartComponent {
+  // Setting up Inputs for passing data through HTML attributes
   @Input({required: true}) title: string|undefined = undefined;
   @Input({required: true}) type: "pie"|"column" = "pie";
+  @Input({required: true}) name: string = "";
   
+  // Declaring properties for chart construction
   Highcharts: typeof Highcharts = Highcharts;
   chartConstructor: string = 'chart';
-
   chartOptions: Highcharts.Options = {}
 
   ngOnChanges() {
+    // Affectation of the chart options after changes passed by Inputs decorator
     this.chartOptions = {
       title: {
         text: this.title,
       },
       series: [{
         type: this.type,
+        name: this.name,
         data: [
           ['Compte courant', 70],
           ['Livret A', 20],
