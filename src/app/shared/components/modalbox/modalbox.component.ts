48b3c3132a6db67b7bc0b7c8de20351d93c3fc51
Ambroise Maire <ambroise.maire7@gmail.com>
2023-10-18 11:10:13 +0200

    feat/dev-data: Add Investment form
    -- Modalbox Component --
    - Builded a form in the dialog box. It askes the name and the amount of the investment. The category is a select field with a default value, which depends on which button the user clicked on.
    - Form builded with FormBuilder and Validators.

--- src/app/shared/components/modalbox/modalbox.component.ts
+++ src/app/shared/components/modalbox/modalbox.component.ts
@@ -1,4 +1,5 @@
 import { Component, Inject } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { ModalCaseInterface } from 'src/app/core/models/switchform';
 
@@ -8,6 +9,15 @@ import { ModalCaseInterface } from 'src/app/core/models/switchform';
   styleUrls: ['./modalbox.component.scss']
 })
 export class ModalboxComponent {
-  constructor(@Inject(MAT_DIALOG_DATA) private dialogData: ModalCaseInterface) {}
+  constructor(@Inject(MAT_DIALOG_DATA) private dialogData: ModalCaseInterface, private fb: FormBuilder) {}
   public reason: ModalCaseInterface = this.dialogData;
+
+  public form: FormGroup = this.fb.group({
+    name: this.fb.control('', Validators.required),
+    amount: this.fb.control('', Validators.required)
+  })
+
+  public onSubmit() {
+    
+  }
 }
